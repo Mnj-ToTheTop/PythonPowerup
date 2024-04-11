b9f89aadeb05cd46a67ca5d34276d1c84492319d
Manjara Raj <153396359+Mnj-ToTheTop@users.noreply.github.com>
2024-04-11 18:46:34 +0530

    Create QR_Generator.py

---
+++ QR_Generator.py
@@ -0,0 +1,9 @@
+import qrcode
+from PIL import Image
+
+print("QR Code Generator")
+info = input("Enter the info to store in QR: ")
+qrcode.make(info)
+image = qrcode.make(info) #QR is stored in image
+image.save("QR_Code.png")
+Image.open("QR_Code.png")
